45dbd546ad20dafd9d4bf1ef2a4d5d62b7908926
NitinpGithub <np4026@gmail.com>
2024-12-07 09:13:07 +0530

    for reverseNumber

--- testometer/ReverseNumber.java
+++ testometer/ReverseNumber.java
@@ -2,11 +2,10 @@ package testometer;
 
 public class ReverseNumber {
     public static void main(String[] args) {
-        int number = 62345;
+        int number = 12324;
         int reminder=0;
         int reverse=0;
 
-/*
         reminder=number%10;
         reverse=reminder+(reverse*10);
         number=number/10;
@@ -26,13 +25,6 @@ public class ReverseNumber {
         reminder=number%10;
         reverse=reminder+(reverse*10);
         number=number/10;
-        */
-        while(number>0)
-        {
-            reminder=number%10;
-            reverse=reminder+(reverse*10);
-            number=number/10;
-        }
 
         System.out.println(reverse);
 
